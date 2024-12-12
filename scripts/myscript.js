b8db3397eb8b02ca406a11c4149740a470bc6b4e
Annette Antony <annetteantony@Anne.local>
2024-12-12 18:22:40 -0500

    edits to d3

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -2,6 +2,32 @@ const margin = {top: 50, right: 50, bottom: 100, left: 50};
 const width = 800 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
+const teamNames = {
+  "71": "Aston Villa",
+  "72": "Everton",
+  "73": "Bournemouth",
+  "74": "Southampton",
+  "75": "Leicester City",
+  "76": "West Bromwich Albion",
+  "77": "Sunderland",
+  "78": "Crytsal Palace",
+  "79": "Norwich City",
+  "80": "Chelsea",
+  "81": "West Ham",
+  "82": "Tottenham Hotspur",
+  "83": "Arsenal",
+  "84": "Swansea City",
+  "85": "Stoke City",
+  "86": "Newcastle United",
+  "87": "Liverpool",
+  "88": "Manchester City",
+  "89": "Manchester United",
+  "90": "Watford",
+  "91": "Hull City",
+  "92": "Burnley",
+  "93": "Middlesbrough"
+};
+
 const svg = d3.select("#plot")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
@@ -73,88 +99,95 @@ d3.csv("https://raw.githubusercontent.com/annette-antony/sportsperformance/refs/
   }
 
   function updateChart(data) {
-    // Sort data by decreasing total goals
-    data = data.slice().sort((a, b) => b.goals - a.goals);
+  // Extract team_ids and convert them to strings for matching with teamNames
+  const sortedTeamIds = data
+    .map(d => d.team_id)          // team_id as is (likely string from CSV)
+    .map(String)                  // ensure they are strings
+    .sort((a, b) => +a - +b);     // sort numerically
 
-    // Update domains after sorting
-    const x = d3.scaleBand().range([0, width]).padding(0.2)
-      .domain(data.map(d => d.team_id));
+  // Update x domain with sorted string IDs
+  const x = d3.scaleBand().range([0, width]).padding(0.2)
+    .domain(sortedTeamIds);
 
-    const maxVal = d3.max(data, d => Math.max(d.goals, d.xG)) || 1;
-    const y = d3.scaleLinear().range([height, 0]).domain([0, maxVal]);
+  const maxVal = d3.max(data, d => Math.max(d.goals, d.xG)) || 1;
+  const y = d3.scaleLinear().range([height, 0]).domain([0, maxVal]);
 
-    // Axes
-    const xAxis = d3.axisBottom(x).tickFormat(d => d);
-    const yAxis = d3.axisLeft(y);
+  const xAxis = d3.axisBottom(x).tickFormat(d => teamNames[d]);
+  const yAxis = d3.axisLeft(y);
 
-    // Create/update axis groups
-    let xAxisG = chartGroup.selectAll(".x-axis").data([null]);
-    xAxisG = xAxisG.enter().append("g").attr("class", "x-axis").merge(xAxisG);
-    xAxisG
-      .attr("transform", `translate(0,${height})`)
-      .transition().duration(750)
-      .call(xAxis);
+  let xAxisG = chartGroup.selectAll(".x-axis").data([null]);
+  xAxisG = xAxisG.enter().append("g").attr("class", "x-axis").merge(xAxisG);
+  xAxisG
+    .attr("transform", `translate(0,${height})`)
+    .transition().duration(750)
+    .call(xAxis);
 
-    let yAxisG = chartGroup.selectAll(".y-axis").data([null]);
-    yAxisG = yAxisG.enter().append("g").attr("class", "y-axis").merge(yAxisG);
-    yAxisG
-      .transition().duration(750)
-      .call(yAxis);
+  let yAxisG = chartGroup.selectAll(".y-axis").data([null]);
+  yAxisG = yAxisG.enter().append("g").attr("class", "y-axis").merge(yAxisG);
+  yAxisG
+    .transition().duration(750)
+    .call(yAxis);
 
-    // Bar group (for grouped bars: goals and xG)
-    const barGroups = chartGroup.selectAll(".bar-group")
-      .data(data, d => d.team_id);
+  // Rotate the x-axis labels for readability
+  xAxisG.selectAll("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-45)")
+    .attr("dx", "-0.5em")
+    .attr("dy", "0.5em");
 
-    barGroups.exit().remove();
+  const barGroups = chartGroup.selectAll(".bar-group")
+    .data(data, d => d.team_id);
 
-    const barGroupsEnter = barGroups.enter()
-      .append("g")
-      .attr("class", "bar-group");
-      
-    const barGroupMerged = barGroupsEnter.merge(barGroups)
-      .attr("transform", d => `translate(${x(d.team_id)},0)`);
+  barGroups.exit().remove();
 
-    // Width for each bar in the group
-    const barWidth = x.bandwidth() / 2;
+  const barGroupsEnter = barGroups.enter()
+    .append("g")
+    .attr("class", "bar-group");
 
-    // Goals bars
-    let goalsBar = barGroupMerged.selectAll(".goals-bar").data(d => [d]);
-    goalsBar.exit().remove();
-    goalsBar = goalsBar.enter()
-      .append("rect")
-      .attr("class", "bar goals-bar")
-      .merge(goalsBar)
-      .attr("x", 0)
-      .attr("width", barWidth)
-      .on("mouseover", (event, d) => showTooltip(event, `Team: ${d.team_id}<br>Goals: ${d.goals}`))
-      .on("mousemove", moveTooltip)
-      .on("mouseout", hideTooltip)
-      .on("click", (event, d) => highlightTeamMatches(d));
+  const barGroupMerged = barGroupsEnter.merge(barGroups)
+    .attr("transform", d => `translate(${x(d.team_id)},0)`);
 
-    goalsBar.transition().duration(2000)
-      .attr("y", d => y(d.goals))
-      .attr("height", d => height - y(d.goals))
-      .attr("fill", "steelblue");
+  const barWidth = x.bandwidth() / 2;
 
-    // xG bars
-    let xgBar = barGroupMerged.selectAll(".xg-bar").data(d => [d]);
-    xgBar.exit().remove();
-    xgBar = xgBar.enter()
-      .append("rect")
-      .attr("class", "bar xg-bar")
-      .merge(xgBar)
-      .attr("x", barWidth)
-      .attr("width", barWidth)
-      .on("mouseover", (event, d) => showTooltip(event, `Team: ${d.team_id}<br>xG: ${d.xG.toFixed(2)}`))
-      .on("mousemove", moveTooltip)
-      .on("mouseout", hideTooltip)
-      .on("click", (event, d) => highlightTeamMatches(d));
+  // Goals bars
+  let goalsBar = barGroupMerged.selectAll(".goals-bar").data(d => [d]);
+  goalsBar.exit().remove();
+  goalsBar = goalsBar.enter()
+    .append("rect")
+    .attr("class", "bar goals-bar")
+    .merge(goalsBar)
+    .attr("x", 0)
+    .attr("width", barWidth)
+    .on("mouseover", (event, d) => showTooltip(event, `Team: ${teamNames[d.team_id]}<br>Goals: ${d.goals}`))
+    .on("mousemove", moveTooltip)
+    .on("mouseout", hideTooltip)
+    .on("click", (event, d) => highlightTeamMatches(d));
 
-    xgBar.transition().duration(2000)
-      .attr("y", d => y(d.xG))
-      .attr("height", d => height - y(d.xG))
-      .attr("fill", "orange");
-  }
+  goalsBar.transition().duration(2000)
+    .attr("y", d => y(d.goals))
+    .attr("height", d => height - y(d.goals))
+    .attr("fill", "steelblue");
+
+  // xG bars
+  let xgBar = barGroupMerged.selectAll(".xg-bar").data(d => [d]);
+  xgBar.exit().remove();
+  xgBar = xgBar.enter()
+    .append("rect")
+    .attr("class", "bar xg-bar")
+    .merge(xgBar)
+    .attr("x", barWidth)
+    .attr("width", barWidth)
+    .on("mouseover", (event, d) => showTooltip(event, `Team: ${teamNames[d.team_id]}<br>xG: ${d.xG.toFixed(2)}`))
+    .on("mousemove", moveTooltip)
+    .on("mouseout", hideTooltip)
+    .on("click", (event, d) => highlightTeamMatches(d));
+
+  xgBar.transition().duration(2000)
+    .attr("y", d => y(d.xG))
+    .attr("height", d => height - y(d.xG))
+    .attr("fill", "orange");
+}
+
 
   function showTooltip(event, htmlContent) {
     tooltip.style("opacity", 1)
